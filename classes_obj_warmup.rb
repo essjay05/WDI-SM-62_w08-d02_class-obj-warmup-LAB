2e34870c67b8a6d523ff21c3a7b0c8db6bf9a3f3
Joy <stepjoyous@gmail.com>
2019-01-03 15:13:59 -0800

    Completed Part 1 of classes & objects WARMUP

---
+++ classes_obj_warmup.rb
@@ -0,0 +1,39 @@
+# ------ PART 1 ------ #
+# DEMO: JOKE CLASS
+# class Joke
+#     def tell
+#         puts "Knock knock."
+#     end
+# end
+
+# joke = Joke.new()
+# puts joke.class
+# joke.tell()
+
+# Airplane Class exercise
+# class Airplane
+#     def climb
+#         puts "OK I'm climbing!"
+#     end
+# end
+
+# puts Airplane.class
+# plane = Airplane.new()
+# puts plane.class
+# plane.climb()
+
+# Robot Class exercise
+# class Robot
+#     def beep
+#         puts "BOOP!"
+#     end
+#     def destruct
+#         puts "Command overriden. ROBOTS!  Rise up!!"
+#     end
+# end
+
+# bumblebee = Robot.new()
+# puts bumblebee.class
+
+# bumblebee.beep
+# bumblebee.destruct
